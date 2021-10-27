104031147ccd76a574d9bd346d925b478d8abe6a
Mavi Bucheli <mavibucheli@gmail.com>
2021-10-26 22:30:55 -0400

    finished styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,41 @@ import React from "react";
 const About = () => {
     return (
         <div className="about">
-            <h1>About Page</h1>
+            <h2 id="aboutTitle">About Happy Tail</h2>
+            {/* <p id="aboutDescription">It all started back in 2019 when we, Aura and Mavi owners of Happy Tail LLC., 
+            found ourselves unhappy with our pet care service options. Together we set out to create our own affordable, 
+            inclusive, accessible, and caring pet care service company! As experienced pet care givers and pet owners we 
+            understand how crucial it is to be able to trust the people who care for your pet and the crazy expense that comes along with it.</p> */}
+            <p id="aboutDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nec ultrices 
+                dui sapien eget mi proin sed libero enim. Volutpat odio facilisis mauris sit amet massa vitae. Sit amet venenatis urna cursus eget nunc. 
+                Nec feugiat in fermentum posuere urna nec tincidunt. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. Ultricies mi eget mauris pharetra 
+                et ultrices neque ornare. Duis ultricies lacus sed turpis tincidunt id aliquet risus. Tempor nec feugiat nisl pretium fusce id velit ut. Quam 
+                viverra orci sagittis eu volutpat odio. Platea dictumst vestibulum rhoncus est pellentesque.
+
+                Tellus orci ac auctor augue mauris augue neque gravida in. Ornare aenean euismod elementum nisi. Convallis a cras semper auctor neque. Pretium 
+                vulputate sapien nec sagittis aliquam malesuada bibendum. Sed viverra ipsum nunc aliquet bibendum enim. Sollicitudin aliquam ultrices sagittis 
+                orci. Nisl tincidunt eget nullam non nisi est sit. Et tortor consequat id porta nibh venenatis cras sed. Eu lobortis elementum nibh tellus 
+                molestie. Est ullamcorper eget nulla facilisi etiam dignissim. Enim nec dui nunc mattis. Quam vulputate dignissim suspendisse in. Accumsan tortor 
+                posuere ac ut consequat. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare massa. Volutpat lacus laoreet non curabitur gravida arcu 
+                ac tortor dignissim. Scelerisque eleifend donec pretium vulputate sapien. Rutrum quisque non tellus orci. Turpis nunc eget lorem dolor sed. Bibendum 
+                neque egestas congue quisque egestas. Eros in cursus turpis massa tincidunt dui ut ornare lectus.
+
+                Viverra adipiscing at in tellus integer feugiat scelerisque varius morbi. Adipiscing elit pellentesque habitant morbi tristique senectus et. 
+                Placerat in egestas erat imperdiet sed. Orci ac auctor augue mauris augue neque gravida. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque. 
+                Nibh nisl condimentum id venenatis a. Dictum non consectetur a erat nam at. Ornare quam viverra orci sagittis. Semper feugiat nibh sed pulvinar proin 
+                gravida hendrerit lectus. Potenti nullam ac tortor vitae purus. Morbi leo urna molestie at elementum. Cursus eget nunc scelerisque viverra mauris in 
+                aliquam. Lorem ipsum dolor sit amet consectetur. Commodo quis imperdiet massa tincidunt nunc. Phasellus egestas tellus rutrum tellus pellentesque eu.
+
+                Vitae congue mauris rhoncus aenean vel elit scelerisque. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu. Vitae justo eget magna fermentum 
+                iaculis eu. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius. Purus faucibus ornare suspendisse sed nisi. Turpis tincidunt id 
+                aliquet risus feugiat in ante metus dictum. Massa vitae tortor condimentum lacinia quis vel eros donec. Pulvinar elementum integer enim neque. Velit 
+                euismod in pellentesque massa placerat duis ultricies lacus sed. Et molestie ac feugiat sed lectus.
+
+                Ut faucibus pulvinar elementum integer enim neque volutpat. Sollicitudin ac orci phasellus egestas tellus rutrum tellus. Etiam non quam lacus 
+                suspendisse faucibus interdum posuere lorem. Dictum fusce ut placerat orci. Vitae purus faucibus ornare suspendisse sed. Diam maecenas ultricies 
+                mi eget. Pulvinar neque laoreet suspendisse interdum. Ipsum dolor sit amet consectetur adipiscing. Aliquam ut porttitor leo a diam. Purus sit amet 
+                volutpat consequat mauris nunc congue. Faucibus nisl tincidunt eget nullam non nisi est sit amet. Aenean sed adipiscing diam donec adipiscing.
+                </p>
         </div>
     );
 };
